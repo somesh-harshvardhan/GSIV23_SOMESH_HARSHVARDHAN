1a9315ca1d34eb58e27bb232a5d17ff5248c67f0
somesh-harshvardhan <somesh.harshvardhan@gmail.com>
2023-08-18 10:43:26 +0530

    file structure changed

--- components/list/Search.js
+++ components/list/Search.js
@@ -13,7 +13,7 @@ const Search = () => {
 
   const {loading} = useSearchContext();
   return (
-    <form className=" flex  items-center justify-start px-3 py-2 bg-disabled w-1/3 rounded-lg">
+    <form className=" flex  items-center justify-start px-3 py-2 bg-disabled w-1/3 max-md:w-2/3 rounded-lg">
       <div>
         <BiSearch
           className=" text-light-gray cursor-pointer hover:text-black"
